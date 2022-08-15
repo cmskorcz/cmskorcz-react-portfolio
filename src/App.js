cfa5616c002d651bfe73f2f7a99c438d645e9774
cmskorcz <chaseskorey@icloud.com>
2022-08-15 14:04:45 -0400

    unstyled about page loads on start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header';
+import About from './components/About';
 
 function App() {
   return (
     <>
     <Header />
     <main>
-
+      <About />
     </main>
     </>
   );
---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+function About() {
+  return (
+    <Container className="mt-3">
+      <h2>About Me</h2>
+      <p>
+        Hi there! My name is Chase Skorczewski, and I am a Michigan State University Coding Bootcamp student. I have a passion for helping others, which lead me to becoming a paramedic. My goal is to use my knowledge of the medical field to eventually develop systems to better streamline patient care.
+      </p>
+      <p>
+        My interests in the coding field include: Front-End, Back-End, Data Analysis, and Internet of Things. After finishing my bootcamp in web development, my next pursuit will be to learn Python.
+      </p>    
+    </Container>
+  )
+}
+
+export default About;
