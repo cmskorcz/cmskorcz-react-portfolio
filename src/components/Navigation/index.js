99363d7ebd3d760f6699ae18fc1b1bac14246f5e
cmskorcz <chaseskorey@icloud.com>
2022-08-15 13:39:55 -0400

    initial commit, added header w/ nav

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function Navigation() {
+  return (
+    <Navbar expand='md' bg='light'>
+      <Container>
+        <Navbar.Brand href="#">cmskorcz</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navigation;
