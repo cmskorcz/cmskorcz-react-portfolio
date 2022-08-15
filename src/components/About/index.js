7675c78ba153a3399ae702a388a68ff07bf80f35
cmskorcz <chaseskorey@icloud.com>
2022-08-15 14:25:20 -0400

    added some styling and img to about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col"
 
 function About() {
   return (
     <Container className="mt-3">
-      <h2>About Me</h2>
-      <p>
-        Hi there! My name is Chase Skorczewski, and I am a Michigan State University Coding Bootcamp student. I have a passion for helping others, which lead me to becoming a paramedic. My goal is to use my knowledge of the medical field to eventually develop systems to better streamline patient care.
-      </p>
-      <p>
-        My interests in the coding field include: Front-End, Back-End, Data Analysis, and Internet of Things. After finishing my bootcamp in web development, my next pursuit will be to learn Python.
-      </p>    
+      <Row>
+        <Col xs={3}><img src={require('../../assets/images/chase-profile-small.jpeg')} /></Col>
+        <Col className="mt-5">
+          <h2>About Me</h2>
+          <p>
+            Hi there! My name is Chase Skorczewski, and I am a Michigan State University Coding Bootcamp student. I have a passion for helping others, which lead me to become a paramedic. My goal is to use my knowledge of the medical field to eventually develop systems to better streamline patient care.
+          </p>
+          <p>
+            My interests in the coding field include: Front-End, Back-End, Data Analysis, and Internet of Things. After finishing my bootcamp in web development, my next pursuit will be to learn Python.
+          </p>    
+        </Col>
+      </Row>
     </Container>
   )
 }
