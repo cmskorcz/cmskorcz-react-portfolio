a9459c6648073560e883083979e52d6a13377e26
cmskorcz <chaseskorey@icloud.com>
2022-08-17 12:58:59 -0400

    handlers built for form with validation and conditional alerts

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -4,9 +4,12 @@ import Form from "react-bootstrap/Form"
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert"
 
 function ContactForm() {
-  const [validated, setValidated] = useState(false)
+  const [validated, setValidated] = useState(false);
+  const [show, setShow] = useState(false);
+  const [showError, setShowError] = useState(false)
 
   const handleValidation = (e) => {
     e.preventDefault()
@@ -17,31 +20,58 @@ function ContactForm() {
     }
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!validated) {
+      e.stopPropagation();
+      setShowError(true)
+    } else {
+      setShow(true)
+    }
+  }
+
   return (
     <Container className="my-3">
+      { show && (
+        <Alert variant="success" onClose={() => setShow(false)} dismissible className="text-center">
+          <Alert.Heading>Thank you for your submission!</Alert.Heading>
+        </Alert>
+      )}
+      { showError && (
+        <Alert variant="danger" onClose={() => setShowError(false)} dismissible className="text-center">
+          <Alert.Heading>Please fill out all fields!</Alert.Heading>
+        </Alert>
+      )}
       <h2>Contact Form</h2>
       <Row>
         <Col md={6}>
           <Form noValidate validated={validated}>
+
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Name:</Form.Label>
               <Form.Control required type="text" placeholder="Enter Name" onBlur={handleValidation}/>
+
               <Form.Control.Feedback type="invalid">Please enter your name!</Form.Control.Feedback>
               <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
             </Form.Group>
+
             <Form.Group className="mb-3" controlId="email">
               <Form.Label>Email:</Form.Label>
               <Form.Control required type="email" placeholder="Enter Email" onBlur={handleValidation} />
+
               <Form.Control.Feedback type="invalid">Please enter your email!</Form.Control.Feedback>
               <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
             </Form.Group>
+
             <Form.Group className="mb-3" controlId="message">
               <Form.Label>Message:</Form.Label>
               <Form.Control as="textarea" required rows={5} onBlur={handleValidation} />
+
               <Form.Control.Feedback type="invalid">Please enter a message!</Form.Control.Feedback>
               <Form.Control.Feedback>Looks Good!</Form.Control.Feedback>
             </Form.Group>
-            <Button type="submit">Submit</Button>
+
+            <Button type="submit" onClick={handleSubmit}>Submit</Button>
           </Form>
         </Col>
       </Row>
