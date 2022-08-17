275710a2ec38e6b3a864425898e9748420242f26
cmskorcz <chaseskorey@icloud.com>
2022-08-16 21:51:41 -0400

    projects display as cards with img and link to site

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Container from "react-bootstrap/Container"
+import Card from "react-bootstrap/Card"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 function Portfolio({ projects }) {
   return (
     <Container>
-      <ul className="mb-5">
+      <Row xs={1} md={2} className="g-4">
         {projects.map(project => (
-          <li key={`${project.title}`}>
-            <img src={require(`../../assets/images/${project.img}`)} alt={`${project.title}`} />
-            <a href={`${project.link}`} target="_blank" rel="noreferrer">{project.title}</a>
-          </li>
+          <Col key={`${project.title}`}>
+            <Card>
+              <Card.Img style={{ objectFit: 'cover', height: '18rem', objectPosition: '0% 0%' }} src={require(`../../assets/images/${project.img}`)} alt={`${project.title}`}/>
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Link href={`${project.link}`} target="_blank" rel="noreferrer">Click to Visit</Card.Link>
+            </Card>
+          </Col>
         ))}
-      </ul>
+      </Row>
     </Container>
   )
 }
