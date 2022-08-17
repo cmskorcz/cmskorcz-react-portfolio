e378621728dbfbb1a721603e272bbdae7e5dadd3
cmskorcz <chaseskorey@icloud.com>
2022-08-17 13:00:42 -0400

    minor card style change

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -9,7 +9,7 @@ function Project({ title, link, repo, img }) {
         <Card.Link href={`${link}`} target="_blank" rel="noreferrer">Click to Visit Site</Card.Link>
         <Card.Link href={`${repo}`} target="_blank" rel="noreferrer">Click to Visit Repo</Card.Link>
       </Card.Body>
-      <Card.Img variant="bottom" style={{ objectFit: 'cover', height: '18rem', objectPosition: '0% 0%' }} src={require(`../../assets/images/${img}`)} alt={`${title}`}/>
+      <Card.Img variant="bottom" style={{ objectFit: 'cover', height: '18rem', objectPosition: '50% 0%' }} src={require(`../../assets/images/${img}`)} alt={`${title}`}/>
     </Card>
   )
 }
