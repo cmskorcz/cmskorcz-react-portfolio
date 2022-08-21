e491f4585b45d155ae424df581ba622882b06424
cmskorcz <chaseskorey@icloud.com>
2022-08-21 16:24:08 -0400

    added linkedin profile link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,8 @@ function Footer() {
       <h2 className="text-light">Contact Information:</h2>
       <ListGroup variant="flush">
         <ListGroup.Item className="bg-dark text-light">Email: <a href="mailto:chaseskorey@gmail.com" className="text-reset">chaseskorey@gmail.com</a></ListGroup.Item>
-        <ListGroup.Item className="bg-dark text-light pb-3">GitHub: <a href="https://github.com/cmskorcz" className="text-reset" target="_blank" rel="noreferrer">cmskorcz</a></ListGroup.Item>
+        <ListGroup.Item className="bg-dark text-light">GitHub: <a href="https://github.com/cmskorcz" className="text-reset" target="_blank" rel="noreferrer">cmskorcz</a></ListGroup.Item>
+        <ListGroup.Item className="bg-dark text-light">LinkedIn: <a href="https://www.linkedin.com/in/chase-skorczewski/" className="text-reset" target="_blank" rel="noreferrer">Chase Skorczewski</a></ListGroup.Item>
       </ListGroup>
     </Container>
   )
